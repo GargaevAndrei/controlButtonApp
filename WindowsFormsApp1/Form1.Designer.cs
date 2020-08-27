cea5d463807936b20335f268ec26362c4af254a2
Andrei_Gargaev <andrei345@yandex.ru>
2020-08-27 15:20:04 +0700

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timerReOpen = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(354, 41);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(174, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(18, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timerReOpen
+            // 
+            this.timerReOpen.Enabled = true;
+            this.timerReOpen.Interval = 250;
+            this.timerReOpen.Tick += new System.EventHandler(this.timerReOpen_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 91);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ButtonApp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timerReOpen;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,242 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string json;
+        Item items;
+        SerialPort port1;
+        int baudrate = 9600;
+        bool A1 = true;
+        bool A2 = true;
+        bool A4 = true;
+        bool isOpenCom = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            OpenCom();
+
+            this.notifyIcon1.MouseDoubleClick += new MouseEventHandler(notifyIcon1_MouseDoubleClick);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+
+            WindowState = FormWindowState.Minimized;
+            //ShowIcon = false;
+            ShowInTaskbar = false;
+            Hide();
+            notifyIcon1.Visible = true;
+            timer1.Start();
+        }
+
+        public class Item
+        {
+            public string portName;
+            public string app1;
+            public string app2;
+            public string app3;
+        }
+
+
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            notifyIcon1.Visible = false;
+            WindowState = FormWindowState.Normal;
+        }
+        
+
+        public void LoadJson()
+        {
+            using (StreamReader r = new StreamReader("file.json"))
+            {
+                json = r.ReadToEnd();
+                items = JsonConvert.DeserializeObject<Item>(json);
+            }
+        }
+
+    
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenCom();            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CloseCom();
+        }
+
+        private void OpenCom()
+        {
+            A4 = true;
+            A2 = true;
+            A1 = true;
+            isOpenCom = true;
+            LoadJson();
+            try
+            {
+                port1 = new SerialPort(items.portName, baudrate);
+                port1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(port1_DataReceived);
+                port1.Open();
+                //this.Invoke(new EventHandler(delegate {
+                    progressBar1.Value = 100;
+                //}));
+            }
+            catch (Exception)
+            {
+                textBox1.Text = "Невозможно открыть последовательный порт " + items.portName.ToString();
+            }
+        }
+
+        private void CloseCom()
+        {
+            port1.Close();
+            port1.Dispose();
+            this.Invoke(new EventHandler(delegate  {
+                progressBar1.Value = 0;
+            }));
+            A4 = false;
+            A2 = false;
+            A1 = false;
+            isOpenCom = false;
+        }
+
+        private void port1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string command = port1.ReadExisting();
+            Process[] proc = Process.GetProcesses();
+
+            foreach (Process process in proc)
+            {
+
+                if ("HDcamera" == process.ProcessName)                
+                    A1 = false;
+                                                 
+                if (items.app1 == process.ProcessName)
+                    A2 = false;               
+
+                if (items.app3 == process.ProcessName)
+                    A4 = false;
+
+            }
+         
+
+            if (command == "A1" && A1)
+            {
+                A1 = false;
+                
+                try
+                {                   
+                    Process.Start(items.app1);
+                    CloseCom();
+                }
+                catch (Exception)
+                {
+                    this.Invoke(new EventHandler(delegate
+                    {
+                        textBox1.Text = "Не удается запустить приложение 1   ";
+                    }));
+                }
+            }
+
+            if (command == "A2" && A2)
+            {
+                A2 = false;
+                try
+                {
+                    Process.Start(items.app2);
+                }
+                catch (Exception)
+                {
+                    this.Invoke(new EventHandler(delegate
+                    {
+                        textBox1.Text = "Не удается запустить приложение 2";
+                    }));
+                }
+            }
+
+            if (command == "A4" && A4)
+            {
+                A4 = false;
+                try
+                {
+                    Process.Start(items.app3);                   
+                }
+                catch (Exception)
+                {
+                    this.Invoke(new EventHandler(delegate
+                    {
+                        textBox1.Text = "Не удается запустить приложение 3";
+                    }));
+                }
+            }
+
+
+        }
+       
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            A4 = true;
+            A2 = true;
+            A1 = true;
+        }
+
+
+        private void timerReOpen_Tick(object sender, EventArgs e)
+        {
+            Process[] proc = Process.GetProcesses();
+            bool isHDcamera = false;
+
+            foreach (Process process in proc)
+            {
+                if (process.ProcessName == "HDcamera")
+                {             
+                    CloseCom();
+                    isHDcamera = true;
+                    break;
+                }                               
+            }
+
+            if (!isHDcamera && !isOpenCom)
+                OpenCom();
+
+
+            /*for(int i=0; i<proc.Length-1; i++)
+            {
+                if (proc[i].ProcessName == "HDcamera")
+                {
+                    flagHDcamera = true;
+                    break;
+                }
+            }*/
+
+            
+        }
+    }
+}
